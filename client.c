ff2beb0f4c01fb4fa5232d61dc1e3f4d9833b061
Dung-Ru Tsai <ldotrg@hotmail.com>
2020-03-08 17:58:11 +0800

    Add the performance relative work

--- client.c
+++ client.c
@@ -3,11 +3,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 #define FIB_DEV "/dev/fibonacci_dev"
-#define MAX_LENGTH 1000
+#define MAX_LENGTH 100
 #define MAX_DIGITS 1000
+#define BILLION 1000000000L
+#define MILLION 1000000L
+
+static long diff_in_ns(struct timespec t1, struct timespec t2)
+{
+    struct timespec diff;
+    if (t2.tv_nsec - t1.tv_nsec < 0) {
+        diff.tv_sec = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + BILLION;
+    } else {
+        diff.tv_sec = t2.tv_sec - t1.tv_sec;
+        diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    }
+    return (diff.tv_sec * BILLION + diff.tv_nsec);
+}
+
 int main()
 {
     long long sz;
@@ -15,6 +32,9 @@ int main()
     char buf[MAX_DIGITS] = "";
     char write_buf[] = "testing writing";
     int offset = MAX_LENGTH;
+    /* Performance util*/
+    struct timespec start, stop;
+    FILE *output_text = fopen("./performance.csv", "w");
 
     int fd = open(FIB_DEV, O_RDWR);
     if (fd < 0) {
@@ -30,12 +50,17 @@ int main()
 
     for (int i = 0; i <= offset; i++) {
         lseek(fd, i, SEEK_SET);
+        clock_gettime(CLOCK_MONOTONIC, &start);
         sz = read(fd, buf, MAX_DIGITS);
+        clock_gettime(CLOCK_MONOTONIC, &stop);
+        double ns = diff_in_ns(start, stop);
+        fprintf(output_text, "%d, %f\n", i, ns);
         printf("Reading from " FIB_DEV
                " at offset %d, returned the sequence "
                "%s.\n",
                i, buf);
     }
+    fclose(output_text);
 
     for (int i = offset; i >= 0; i--) {
         lseek(fd, i, SEEK_SET);
@@ -45,7 +70,6 @@ int main()
                "%s.\n",
                i, buf);
     }
-
     close(fd);
     return 0;
 }
